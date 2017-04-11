494ef667d05289bbee0cd8929a285e3861352903
Sanpi <sanpi@homecomputing.fr>
2017-04-11 12:28:13 +0200

    [digital] Add digital::State enum

--- src/digital.rs
+++ src/digital.rs
@@ -1,5 +1,34 @@
 use socket::Socket;
 
+#[derive(Debug, PartialEq)]
+pub enum State {
+    LOW,
+    HIGH,
+}
+
+impl ::std::fmt::Display for State {
+    fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
+        let display = match self {
+            &State::LOW => "0",
+            &State::HIGH => "1",
+        };
+
+        write!(f, "{}", display)
+    }
+}
+
+impl ::std::str::FromStr for State {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "0" => Ok(State::LOW),
+            "1" => Ok(State::HIGH),
+            _ => Err(()),
+        }
+    }
+}
+
 pub enum Direction {
     OUT,
     IN,
@@ -31,11 +60,11 @@ impl Digital {
         self.socket.send(format!("DIG:PIN:DIR {},{}", direction, pin));
     }
 
-    pub fn set_state(&mut self, pin: &str, state: u8) {
+    pub fn set_state(&mut self, pin: &str, state: State) {
         self.socket.send(format!("DIG:PIN {},{}", pin, state));
     }
 
-    pub fn get_state(&mut self, pin: &str) -> u8 {
+    pub fn get_state(&mut self, pin: &str) -> State {
         self.socket.send(format!("DIG:PIN? {}", pin));
 
         self.socket.receive()
@@ -66,15 +95,15 @@ mod test {
     fn test_set_state() {
         let (rx, mut digital) = create_digital();
 
-        digital.set_state("DIO0_N", 1);
-        assert_eq!("DIG:PIN DIO0_N,1\r\n", rx.recv().unwrap());
+        digital.set_state("DIO0_N", ::digital::State::LOW);
+        assert_eq!("DIG:PIN DIO0_N,0\r\n", rx.recv().unwrap());
     }
 
     #[test]
     fn test_get_state() {
         let (_, mut digital) = create_digital();
 
-        assert_eq!(digital.get_state("DIO0_N"), 1);
+        assert_eq!(digital.get_state("DIO0_N"), ::digital::State::HIGH);
     }
 
     fn create_digital() -> (::std::sync::mpsc::Receiver<String>, ::digital::Digital) {
